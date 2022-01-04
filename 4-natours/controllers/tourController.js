51fece6c3bfe20a8d7461ae11d85e8642656c591
thuynt0803 <thuy83.tmu@gmail.com>
2022-01-04 21:47:51 +0700

    Update API get tours: filter, sort, pagination

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,59 +1,22 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+};
 
 exports.getAllTours = async(req, res) => {
     try {
-        console.log(req.query);
-
-        // BIULD QUERY
-        // 1A) Filtering
-        const queryObj = {...req.query };
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach((el) => delete queryObj[el]);
-
-        // 1B) Advanced filtering
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-        console.log(JSON.parse(queryStr));
-
-        // { difficulty: 'easy', duration: { $gte: 5 } }
-        // { difficulty: 'easy', duration: { gte: '5' } }
-        // gte, gt, lte, lt
-
-        let query = Tour.find(JSON.parse(queryStr));
-
-        // 2) Sorting
-        if (req.query.sort) {
-            // Sort('price, ratingsAverage') : Sort theo dkien 2 khi dkien 1 co values bang nhau
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy);
-        } else {
-            // In case that the user does not specify any sort field in the URL query string => auto sort by the createdAt
-            query = query.sort('-createdAt');
-        }
-
-        // 3) Field limiting
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-        } else {
-            query = query.select('-__v');
-        }
-
-        // 4) Pagination || movie 18 (continue)
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limiy * 1 || 100;
-        const skip = (page - 1) * limit;
-
-        //  page=2&limit=10, 1-10 page 1, 11-20 page 2
-        query = query.skip(skip).limit(limit);
-
         // EXECUTE QUERY
-        const tours = await query;
-        // const tours = await Tour.find()
-        //     .where('duration')
-        //     .equals(5)
-        //     .where('difficulty')
-        //     .equals('easy');
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        const tours = await features.query;
 
         // SEND RESPONSE
         res.status(200).json({
